a31f7a5adeff1824f4148f2ce0637f691c1cedc4
duyanh14 <duyanhnguyen2001@gmail.com>
2022-06-15 10:13:31 +0700

    Update blue&empty bottle

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -14,6 +14,8 @@ public class Paddle : MonoBehaviour
     // Unity units of the WIDTH of the screen (e.g. 16)
     [SerializeField] public float screenWidthUnits = 16;
 
+    private float paddleMoveSpeed = 5f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -25,7 +27,8 @@ public class Paddle : MonoBehaviour
     void Update()
     {
         var relativePosX = ConvertPixelToRelativePosition(pixelPosition: Input.mousePosition.x, Screen.width);
-        transform.position = GetUpdatedPaddlePosition(relativePosX);
+        transform.position = Vector2.MoveTowards(transform.position, GetUpdatedPaddlePosition(relativePosX),
+            paddleMoveSpeed * Time.deltaTime);
     }
 
     public Vector2 GetUpdatedPaddlePosition(float relativePosX)
@@ -51,9 +54,9 @@ public class Paddle : MonoBehaviour
         }
 
         Potion p = other.gameObject.GetComponent<Potion>();
-        
+
         Destroy(other.gameObject);
-        
+
         if (p.PotionSelect == 1)
         {
             touchHeartPotion();
@@ -79,21 +82,23 @@ public class Paddle : MonoBehaviour
         {
             return;
         }
+
         gameSession.PlayerLives++;
     }
 
     private DateTime paddleScaleTime;
+
     private void touchGearPotion()
     {
         paddleScaleTime = DateTime.Now;
         transform.localScale = new Vector3(2, 1, 1);
         StartCoroutine(resetPaddleScale(10.0f));
     }
-    
+
     IEnumerator resetPaddleScale(float seconds)
     {
         yield return new WaitForSeconds(seconds);
-        double s =  Math.Abs((DateTime.Now - paddleScaleTime).TotalSeconds);
+        double s = Math.Abs((DateTime.Now - paddleScaleTime).TotalSeconds);
         if (s < 10)
         {
             StartCoroutine(resetPaddleScale((float) s));
@@ -104,12 +109,57 @@ public class Paddle : MonoBehaviour
         }
     }
 
+    private DateTime paddleMoveSpeedTime;
+    private int touchBlueBottleN = 0;
+
     private void touchBlueBottle()
     {
-        
+        if (touchBlueBottleN == 2)
+        {
+            return;
+        }
+
+        if (touchBlueBottleN == 1)
+        {
+            double s = Math.Abs((DateTime.Now - paddleMoveSpeedTime).TotalSeconds);
+            if (s < 5)
+            {
+                return;
+            }
+        }
+
+        paddleMoveSpeed += (float) (5 * 0.3);
+        touchBlueBottleN += 1;
+        paddleMoveSpeedTime = DateTime.Now;
+        if (touchBlueBottleN == 1)
+        {
+            StartCoroutine(resetPaddleMoveSpeed(10.0f));
+        }
+        Debug.Log("paddleMoveSpeed "+paddleMoveSpeed);
     }
+
+    IEnumerator resetPaddleMoveSpeed(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        Debug.Log(123);
+        if (touchBlueBottleN > 0)
+        {
+            paddleMoveSpeed -= (float) (5 * 0.3);
+            touchBlueBottleN -= 1;
+            double s = Math.Abs((DateTime.Now - paddleMoveSpeedTime).TotalSeconds);
+            if (s < 10)
+            {
+                StartCoroutine(resetPaddleMoveSpeed((float) s));
+            }
+            Debug.Log("paddleMoveSpeed "+paddleMoveSpeed);
+        }
+    }
+
     private void touchEmptyBottle()
     {
+        paddleMoveSpeed = 5;
+        touchBlueBottleN = 0;
         
+        transform.localScale = new Vector3(1, 1, 1);
     }
 }
